cd68f4da5e1754ac8deebf5237a5a192aa2312f6
Michael Mironov <nemiron94@gmail.com>
2023-04-24 21:55:49 +0300

    added extension for clearing browser cookies

--- rangiffler-e-2-e-tests/src/test/java/jupiter/annotation/meta/WebTest.java
+++ rangiffler-e-2-e-tests/src/test/java/jupiter/annotation/meta/WebTest.java
@@ -2,6 +2,7 @@ package jupiter.annotation.meta;
 
 import io.qameta.allure.junit5.AllureJunit5;
 import jupiter.extension.BrowserExtension;
+import jupiter.extension.ClearCookiesAndSessionExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.lang.annotation.ElementType;
@@ -11,6 +12,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@ExtendWith({AllureJunit5.class, BrowserExtension.class})
+@ExtendWith({AllureJunit5.class, ClearCookiesAndSessionExtension.class, BrowserExtension.class})
 public @interface WebTest {
 }
