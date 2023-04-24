991ad0658fd6f98f9a89a7d0694017d85ded22c0
Michael Mironov <nemiron94@gmail.com>
2023-04-24 22:03:53 +0300

    added userdata tests

--- rangiffler-e-2-e-tests/src/test/java/tests/rest/UserDataUsersRestTest.java
+++ rangiffler-e-2-e-tests/src/test/java/tests/rest/UserDataUsersRestTest.java
@@ -3,8 +3,6 @@ package tests.rest;
 import api.UserdataClient;
 import io.qameta.allure.AllureId;
 import io.qameta.allure.Epic;
-import io.qameta.allure.Severity;
-import io.qameta.allure.SeverityLevel;
 import jupiter.annotation.GenerateUser;
 import jupiter.annotation.meta.User;
 import model.UserJson;
@@ -26,7 +24,6 @@ public class UserDataUsersRestTest extends BaseRestTest {
     @Test
     @AllureId("300")
     @Tag("REST")
-    @Severity(SeverityLevel.CRITICAL)
     @GenerateUser()
     @DisplayName("REST: For a new user, information from Userdata service should be returned with default values")
     void currentUserTest(@User(selector = METHOD) UserJson user) throws Exception {
@@ -44,7 +41,6 @@ public class UserDataUsersRestTest extends BaseRestTest {
     @Test
     @AllureId("301")
     @Tag("REST")
-    @Severity(SeverityLevel.CRITICAL)
     @GenerateUser()
     @DisplayName("REST: When updating a user, the values in Userdata service must be saved")
     void updateUserTest(@User(selector = METHOD) UserJson user) throws Exception {
@@ -76,7 +72,6 @@ public class UserDataUsersRestTest extends BaseRestTest {
     @DisplayName("REST: List of all system users must not be empty")
     @AllureId("303")
     @Tag("REST")
-    @Severity(SeverityLevel.CRITICAL)
     @GenerateUser()
     void allUsersTest(@User(selector = METHOD) UserJson user) throws Exception {
         final List<UserJson> allUsersResponse = userdataClient.allUsers(user.getUsername());
