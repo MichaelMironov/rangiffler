457d793fbdd1b8f51777b7e83c1526611918979e
Michael Mironov <nemiron94@gmail.com>
2023-04-24 22:03:25 +0300

    added photo tests

--- rangiffler-e-2-e-tests/src/test/java/tests/web/PhotoTest.java
+++ rangiffler-e-2-e-tests/src/test/java/tests/web/PhotoTest.java
@@ -6,9 +6,11 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import jupiter.annotation.ApiLogin;
+import jupiter.annotation.Friends;
 import jupiter.annotation.GeneratePhoto;
 import jupiter.annotation.GenerateUser;
 import jupiter.annotation.meta.User;
+import model.PhotoJson;
 import model.UserJson;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -18,9 +20,12 @@ import page.MainPage;
 
 import java.io.IOException;
 
+import static jupiter.extension.CreateUserExtension.Selector.NESTED;
+
+@SuppressWarnings("ALL")
 @Epic("[WEB][Frontend] Photo | DB preconditions")
 @DisplayName("[WEB][Frontend] Photo | DB preconditions")
-public class PhotoTest extends BaseWebTest {
+class PhotoTest extends BaseWebTest {
 
     @Test
     @AllureId("600")
@@ -28,7 +33,7 @@ public class PhotoTest extends BaseWebTest {
     @Severity(SeverityLevel.CRITICAL)
     @DisplayName("WEB: User can add country photo")
     @ApiLogin(user = @GenerateUser)
-    public void userCanAddPhoto() throws IOException {
+    void userCanAddPhoto() throws IOException {
 
         String photo = "data/img/Tunis.jpg";
 
@@ -45,16 +50,14 @@ public class PhotoTest extends BaseWebTest {
     @Test
     @AllureId("601")
     @Tags({@Tag("WEB"), @Tag("DB")})
-    @Severity(SeverityLevel.CRITICAL)
     @DisplayName("WEB: User can edit country photo")
     @ApiLogin(user = @GenerateUser
             (
                     photo = @GeneratePhoto(country = "United Arab Emirates", description = "holiday")
             ))
-    public void userCanEditAddedPhoto(@User UserJson user) throws IOException {
+    void userCanEditAddedPhoto(@User(selector = NESTED) UserJson user) throws IOException {
 
-        String generatedCountry = user.getPhotos().get(0).getCountryJson().getName();
-        String generatedDescription = user.getPhotos().get(0).getDescription();
+        final PhotoJson generatedPhoto = user.getPhotos().get(0);
 
         String newCountry = "Democratic Republic of the Congo";
         String newDescription = "travel";
@@ -62,8 +65,8 @@ public class PhotoTest extends BaseWebTest {
         new MainPage()
                 .open()
                 .waitForPageLoaded()
-                .checkPhoto(generatedDescription, generatedCountry)
-                .selectPhoto(generatedDescription)
+                .checkPhoto(generatedPhoto.getDescription(), generatedPhoto.getCountryJson().getName())
+                .selectPhoto(generatedPhoto.getDescription())
                 .editPhoto()
                 .setCountry(newCountry).setDescription(newDescription)
                 .savePhoto()
@@ -71,4 +74,44 @@ public class PhotoTest extends BaseWebTest {
                 .checkPhoto(newDescription, newCountry);
 
     }
+
+    @Test
+    @AllureId("602")
+    @Tags({@Tag("WEB"), @Tag("DB")})
+    @DisplayName("WEB: User can remove country photo")
+    @ApiLogin(user = @GenerateUser
+            (
+                    photo = @GeneratePhoto(country = "Democratic Republic of the Congo", description = "holiday")
+            ))
+    void userCanRemoveAddedPhoto(@User(selector = NESTED) UserJson user) throws IOException {
+
+        final PhotoJson generatedPhoto = user.getPhotos().get(0);
+
+        new MainPage()
+                .open()
+                .waitForPageLoaded()
+                .selectPhoto(generatedPhoto.getDescription())
+                .removePhoto()
+                .photoCountShouldBeEqual(0);
+
+    }
+
+    @Test
+    @AllureId("603")
+    @Tags({@Tag("WEB"), @Tag("DB")})
+    @DisplayName("WEB: User can see friend country photo")
+    @ApiLogin(user = @GenerateUser(
+            friends = @Friends(count = 1,
+                    photo = @GeneratePhoto(country = "Canada", description = "travel")
+            )))
+    void userCanSeeFriendCountryPhoto(@User(selector = NESTED) UserJson user) throws IOException {
+
+        new MainPage()
+                .open()
+                .waitForPageLoaded()
+                .toFriendsTravels()
+                .checkPhoto("travel", "Canada");
+
+    }
+
 }
