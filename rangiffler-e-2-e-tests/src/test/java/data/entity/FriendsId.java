d9071e72d10af1fefebad96f95bd8b10a59addff
Michael Mironov <nemiron94@gmail.com>
2023-04-24 22:08:21 +0300

    added friends entity

---
+++ rangiffler-e-2-e-tests/src/test/java/data/entity/FriendsId.java
@@ -0,0 +1,12 @@
+package data.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+public class FriendsId implements Serializable {
+    private UUID user;
+    private UUID friend;
+}
